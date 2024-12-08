6cf9056476df50f2d12936172b22f6e68e6197ad
DzinnnXn <andresilvax444@gmail.com>
2024-12-08 17:34:21 -0300

    Cadastro light e darkmode

--- Mobile/app/(tabs)/telas/cadastro.tsx
+++ Mobile/app/(tabs)/telas/cadastro.tsx
@@ -8,15 +8,20 @@ import {
   Alert,
   ScrollView,
   KeyboardAvoidingView,
+  useColorScheme,
+  StatusBar,
 } from 'react-native';
-import axios from 'axios';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import axios from 'axios';
 
 interface CadastroProps {
   onNavigate: (screen: string) => void;
 }
 
 const Cadastro: React.FC<CadastroProps> = ({ onNavigate }) => {
+  const colorScheme = useColorScheme();
+  const themeStyles = colorScheme === 'dark' ? darkTheme : lightTheme;
+
   const [formData, setFormData] = useState({
     first_name: '',
     last_name: '',
@@ -33,19 +38,17 @@ const Cadastro: React.FC<CadastroProps> = ({ onNavigate }) => {
   };
 
   const onSubmit = async () => {
-    // Verificação de campos obrigatórios
     if (!formData.first_name || !formData.last_name || !formData.email || !formData.password) {
       Alert.alert('Erro', 'Todos os campos são obrigatórios.');
       return;
     }
-  
-    // Verificação do formato do e-mail
+
     const emailRegex = /^[a-zA-Z0-9._%+-]+@gmail\.com$/;
     if (!emailRegex.test(formData.email)) {
       Alert.alert('Erro', 'O e-mail deve ser um endereço Gmail válido (ex: usuario@gmail.com).');
       return;
     }
-  
+
     try {
       const response = await axios.post('https://patrimoniosemordem.nestguard.com.br/api/cadastro/', formData);
       if (response.status === 200) {
@@ -53,70 +56,67 @@ const Cadastro: React.FC<CadastroProps> = ({ onNavigate }) => {
         onNavigate('Login');
       }
     } catch (error) {
-      if (axios.isAxiosError(error)) {
-        // Tratamento de erros específicos com base no código de status
-        if (error.response) {
-          switch (error.response.status) {
-            case 400: // Erros de validação, como campos duplicados ou inválidos
-              Alert.alert('Erro', error.response.data.detail || 'Dados inválidos. Verifique as informações e tente novamente.');
-              break;
-            case 409: // Conflito, como e-mail ou usuário duplicado
-              Alert.alert('Erro', 'Usuário ou e-mail já cadastrado.');
-              break;
-            default:
-              Alert.alert('Erro', 'Não foi possível realizar o cadastro. Verifique os dados e tente novamente.');
-              break;
-          }
+      if (axios.isAxiosError(error) && error.response) {
+        const { status } = error.response;
+        if (status === 400) {
+          Alert.alert('Erro', error.response.data.detail || 'Dados inválidos. Verifique as informações.');
+        } else if (status === 409) {
+          Alert.alert('Erro', 'Usuário ou e-mail já cadastrado.');
         } else {
-          console.error('Erro na requisição:', error.message);
-          Alert.alert('Erro', 'Erro inesperado. Tente novamente mais tarde.');
+          Alert.alert('Erro', 'Não foi possível realizar o cadastro. Tente novamente.');
         }
       } else {
-        console.error('Erro inesperado:', error);
         Alert.alert('Erro', 'Ocorreu um erro inesperado. Tente novamente mais tarde.');
       }
     }
   };
-  
 
   return (
-    <KeyboardAvoidingView style={styles.container} behavior="padding">
+    <KeyboardAvoidingView style={[styles.container, themeStyles.container]} behavior="padding">
+      <StatusBar
+        backgroundColor={themeStyles.container.backgroundColor}
+        barStyle={colorScheme === 'dark' ? 'light-content' : 'dark-content'}
+      />
       <ScrollView contentContainerStyle={styles.scrollContent}>
-        {/* Botão de Voltar */}
         <TouchableOpacity style={styles.backButton} onPress={() => onNavigate('ServiceHome')}>
-          <Icon name="arrow-left" size={20} color="#333" />
+          <Icon name="arrow-left" size={20} color={themeStyles.icon.color} />
         </TouchableOpacity>
 
-        <Text style={styles.title}>Cadastro de Usuário</Text>
+        <Text style={[styles.title, themeStyles.tittleText]}>Cadastro de Usuário</Text>
 
         <TextInput
-          style={styles.input}
+          style={[styles.input, themeStyles.input]}
           placeholder="Nome"
+          placeholderTextColor={themeStyles.placeholder.color}
           value={formData.first_name}
           onChangeText={(text) => handleInputChange('first_name', text)}
         />
         <TextInput
-          style={styles.input}
+          style={[styles.input, themeStyles.input]}
           placeholder="Sobrenome"
+          placeholderTextColor={themeStyles.placeholder.color}
           value={formData.last_name}
           onChangeText={(text) => handleInputChange('last_name', text)}
         />
         <TextInput
-          style={styles.input}
+          style={[styles.input, themeStyles.input]}
           placeholder="Usuário"
+          placeholderTextColor={themeStyles.placeholder.color}
           value={formData.user}
           onChangeText={(text) => handleInputChange('user', text)}
         />
         <TextInput
-          style={styles.input}
+          style={[styles.input, themeStyles.input]}
           placeholder="Email"
           keyboardType="email-address"
+          placeholderTextColor={themeStyles.placeholder.color}
           value={formData.email}
           onChangeText={(text) => handleInputChange('email', text)}
         />
         <TextInput
-          style={styles.input}
+          style={[styles.input, themeStyles.input]}
           placeholder="Sala"
+          placeholderTextColor={themeStyles.placeholder.color}
           value={formData.sala}
           onChangeText={(text) => handleInputChange('sala', text)}
         />
@@ -125,121 +125,92 @@ const Cadastro: React.FC<CadastroProps> = ({ onNavigate }) => {
           <TouchableOpacity
             style={[
               styles.groupButton,
-              formData.group === 'Coordenador' && styles.groupButtonSelected,
+              formData.group === 'Coordenador' ? themeStyles.groupButtonSelected : themeStyles.groupButton,
             ]}
             onPress={() => handleInputChange('group', 'Coordenador')}
           >
-            <Text style={styles.groupText}>Coordenador</Text>
+            <Text style={[styles.groupText, themeStyles.text]}>Coordenador</Text>
           </TouchableOpacity>
           <TouchableOpacity
             style={[
               styles.groupButton,
-              formData.group === 'Professor' && styles.groupButtonSelected,
+              formData.group === 'Professor' ? themeStyles.groupButtonSelected : themeStyles.groupButton,
             ]}
             onPress={() => handleInputChange('group', 'Professor')}
           >
-            <Text style={styles.groupText}>Professor</Text>
+            <Text style={[styles.groupText, themeStyles.text]}>Professor</Text>
           </TouchableOpacity>
         </View>
 
-        <View style={styles.passwordContainer}>
+        <View style={[
+          styles.passwordContainer,
+          themeStyles.input, // Aplica o estilo de input ao container
+          { borderWidth: 1, borderColor: themeStyles.input.borderColor }, // Adiciona borda ao container
+        ]}
+        >
           <TextInput
             style={styles.passwordInput}
             placeholder="Senha"
             secureTextEntry={!showPassword}
+            placeholderTextColor={themeStyles.placeholder.color}
             value={formData.password}
             onChangeText={(text) => handleInputChange('password', text)}
           />
           <TouchableOpacity onPress={() => setShowPassword(!showPassword)}>
-            <Icon name={showPassword ? 'eye' : 'eye-slash'} size={20} color="#333" />
+            <Icon name={showPassword ? 'eye' : 'eye-slash'} size={40} color={themeStyles.icon.color} />
           </TouchableOpacity>
         </View>
 
-        <TouchableOpacity onPress={onSubmit} style={styles.button}>
-          <Text style={styles.buttonText}>Cadastrar</Text>
+        <TouchableOpacity onPress={onSubmit} style={[styles.button, themeStyles.button]}>
+          <Text style={[styles.buttonText, themeStyles.buttonText]}>Cadastrar</Text>
         </TouchableOpacity>
       </ScrollView>
     </KeyboardAvoidingView>
   );
 };
 
+// Estilos gerais
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#FFFFFF',
-  },
-  scrollContent: {
-    flexGrow: 1,
-    justifyContent: 'flex-start', 
-    paddingHorizontal: 20,
-    paddingTop: 60, 
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    marginBottom: 15, // Ajustando a margem inferior
-    color: '#333333',
-  },
-  input: {
-    height: 45,
-    borderColor: '#CCC',
-    borderWidth: 1,
-    marginBottom: 15,
-    paddingHorizontal: 10,
-    borderRadius: 5,
-  },
-  groupContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    marginBottom: 20,
-  },
-  groupButton: {
-    flex: 1,
-    paddingVertical: 10,
-    borderWidth: 1,
-    borderColor: '#CCC',
-    borderRadius: 5,
-    alignItems: 'center',
-    marginHorizontal: 5,
-  },
-  groupButtonSelected: {
-    backgroundColor: '#B22222',
-    borderColor: '#B22222',
-  },
-  groupText: {
-    color: '#FFFFFF',
-    fontWeight: 'bold',
-  },
-  passwordContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    borderColor: '#CCC',
-    borderWidth: 1,
-    borderRadius: 5,
-    paddingHorizontal: 10,
-    marginBottom: 20,
-  },
-  passwordInput: {
-    flex: 1,
-  },
-  button: {
-    backgroundColor: '#B22222',
-    paddingVertical: 12,
-    borderRadius: 5,
-    alignItems: 'center',
-    marginTop: 10,
-  },
-  buttonText: {
-    color: '#FFFFFF',
-    fontWeight: 'bold',
-  },
-  backButton: {
-    position: 'absolute',
-    top: 40,
-    left: 20,
-    zIndex: 1,
-  },
+  container: { flex: 1 },
+  scrollContent: { flexGrow: 1, padding: 20, paddingTop: 60 },
+  title: { fontSize: 20, fontWeight: 'bold', textAlign: 'center', marginBottom: 20 },
+  input: { height: 45, borderWidth: 1, marginBottom: 15, paddingHorizontal: 10, borderRadius: 5 },
+  groupContainer: { flexDirection: 'row', justifyContent: 'space-between', marginBottom: 20 },
+  groupButton: { flex: 1, paddingVertical: 10, borderRadius: 5, alignItems: 'center', marginHorizontal: 5 },
+  groupText: { fontSize: 14, fontWeight: 'bold' }, // Adicionamos este estilo
+  passwordContainer: { flexDirection: 'row', alignItems: 'center', borderRadius: 5, paddingHorizontal: 10, marginBottom: 20 },
+  passwordInput: { flex: 1 },
+  button: { paddingVertical: 12, borderRadius: 5, alignItems: 'center', marginTop: 10 },
+  buttonText: { fontWeight: 'bold' },
+  backButton: { position: 'absolute', top: 40, left: 20 },
+});
+
+// Tema claro
+const lightTheme = StyleSheet.create({
+  container: { backgroundColor: '#fff' },
+  tittleText: { color: '#000' },
+  text: { color: '#fff' },
+  input: { backgroundColor: '#fff', borderColor: '#ccc' },
+  placeholder: { color: '#888' },
+  button: { backgroundColor: '#b22222' },
+  buttonText: { color: '#fff' },
+  groupButton: { backgroundColor: '#5c5c5c', borderColor: '#ccc' },
+  groupButtonSelected: { backgroundColor: '#b22222', borderColor: '#b22222' },
+  icon: { color: '#333' },
+});
+
+// Tema escuro
+const darkTheme = StyleSheet.create({
+  container: { backgroundColor: '#000' },
+  tittleText: { color: '#fff' },
+  text: { color: '#fff' },
+  input: { backgroundColor: '#333', borderColor: '#555' },
+  placeholder: { color: '#aaa' },
+  button: { backgroundColor: '#8B0000' },
+  buttonText: { color: '#eee' },
+  groupButton: { backgroundColor: '#333', borderColor: '#555' },
+  groupButtonSelected: { backgroundColor: '#8B0000', borderColor: '#b22222' },
+  icon: { color: '#eee' },
 });
 
 export default Cadastro;
